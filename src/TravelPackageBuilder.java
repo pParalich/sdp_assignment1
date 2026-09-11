b3f9092b6394cca17afe3e4eab0018f039928e2b
First <abaminsur@gmail.com>
2026-09-12 03:04:09 +0500

    Implement Builder pattern for Product(TravelPackage)

---
+++ src/TravelPackageBuilder.java
@@ -0,0 +1,6 @@
+public interface TravelPackageBuilder {
+    TravelPackageBuilder setCountry(String country);
+    TravelPackageBuilder setCity(String city);
+    TravelPackageBuilder setHotel(String hotel);
+    TravelPackageBuilder setTransport(String transport);
+}
